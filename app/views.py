6b202f3302b9b8f88c1fd8bd73f875028a21e816
satoru-mustang <jt874wm258@gmail.com>
2022-10-10 20:31:29 +0900

    heroku

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.views.generic.base import View
+from django.http.response import HttpResponse
+
+
+class CallbackView(View):
+  def get(self, request, *args, **kwargs):
+    return HttpResponse('OK')
